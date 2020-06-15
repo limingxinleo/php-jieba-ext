34da05d5dd8a1f73bf26b5f97a845bde1569cf2e
李铭昕 <715557344@qq.com>
2020-06-15 14:20:13 +0800

    Update jieba.c

--- jieba.c
+++ jieba.c
@@ -22,6 +22,80 @@ const char* USER_DICT = "/Users/limx/Applications/GitHub/limingxinleo/php-jieba-
 const char* IDF_PATH = "/Users/limx/Applications/GitHub/limingxinleo/php-jieba-ext/dict/idf.utf8";
 const char* STOP_WORDS_PATH = "/Users/limx/Applications/GitHub/limingxinleo/php-jieba-ext/dict/stop_words.utf8";
 
+zend_class_entry *jieba_ce;
+
+PHP_METHOD(PHPJieba, cut);
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_php_jieba_cut, 0, 0, 2)
+    ZEND_ARG_INFO(0, keyword)
+    ZEND_ARG_INFO(0, hmm)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_php_jieba_contruct, 0, 0, 5)
+    ZEND_ARG_INFO(0, dict)
+    ZEND_ARG_INFO(0, hmm)
+    ZEND_ARG_INFO(0, user)
+    ZEND_ARG_INFO(0, idf)
+    ZEND_ARG_INFO(0, stop_words)
+ZEND_END_ARG_INFO()
+
+const zend_function_entry php_jieba_methods[] = {
+    PHP_ME(PHPJieba, __contruct, arginfo_php_jieba_contruct, ZEND_ACC_PUBLIC)
+    PHP_ME(PHPJieba, cut, arginfo_php_jieba_cut, ZEND_ACC_PUBLIC)
+    {NULL, NULL, NULL}
+}
+
+PHP_MINIT_FUNCTION(PHPJieba)
+{
+zend_class_entry ce;
+INIT_CLASS_ENTRY(ce, "PHPJieba", php_jieba_methods);
+
+jieba_ce = zend_register_internal_class(&ce);
+
+zend_declare_property_null(jieba_ce, "dict", sizeof("dict") - 1, ZEND_ACC_PUBLIC);
+zend_declare_property_null(jieba_ce, "hmm", sizeof("hmm") - 1, ZEND_ACC_PUBLIC);
+zend_declare_property_null(jieba_ce, "user", sizeof("user") - 1, ZEND_ACC_PUBLIC);
+zend_declare_property_null(jieba_ce, "idf", sizeof("idf") - 1, ZEND_ACC_PUBLIC);
+zend_declare_property_null(jieba_ce, "stop_words", sizeof("stop_words") - 1, ZEND_ACC_PUBLIC);
+}
+
+PHP_METHOD(PHPJieba, __contruct)
+{
+    zend_string *dict;
+    zend_string *hmm;
+    zend_string *user;
+    zend_string *idf;
+    zend_string *stop_words;
+
+    ZEND_PARSE_PARAMETERS_START(5, 5)
+        Z_PARAM_STR(dict)
+        Z_PARAM_STR(hmm)
+        Z_PARAM_STR(user)
+        Z_PARAM_STR(idf)
+        Z_PARAM_STR(stop_words)
+    ZEND_PARSE_PARAMETERS_END();
+
+    zend_update_property_string(jieba_ce,  getThis(), "dict", sizeof("dict") - 1, (char *)dict);
+    zend_update_property_string(jieba_ce,  getThis(), "hmm", sizeof("hmm") - 1, (char *)hmm);
+    zend_update_property_string(jieba_ce,  getThis(), "user", sizeof("user") - 1, (char *)user);
+    zend_update_property_string(jieba_ce,  getThis(), "idf", sizeof("idf") - 1, (char *)idf);
+    zend_update_property_string(jieba_ce,  getThis(), "stop_words", sizeof("stop_words") - 1, (char *)stop_words);
+}
+
+PHP_METHOD(PHPJieba, cut)
+{
+//    char *love;
+//    zend_string *keyword;
+//    size_t love_len;
+//
+//    ZEND_PARSE_PARAMETERS_START(1, 2)
+//        Z_PARAM_STR(keyword)
+//        Z_PARAM_OPTIONAL
+//        Z_PARAM_ZVAL_EX(value, 0, 1)
+//    ZEND_PARSE_PARAMETERS_END();
+}
+
+
 /* {{{ void jieba_test1()
  */
 PHP_FUNCTION(jieba_test1)
