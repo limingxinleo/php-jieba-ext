c3e0ea84386b843281d3ccbbf54e6c8139cbe15c
李铭昕 <715557344@qq.com>
2020-06-15 18:55:59 +0800

    Update

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 export PHP_VERSION=7.2 && docker-compose build jieba-ext
 docker run --name jieba7.2 hyperf/jieba:7.2
 docker cp jieba7.2:/opt/www/modules/jieba.so build/alpine/7.2/jieba.so
+docker rm jieba7.2
--- examples/server.php
+++ examples/server.php
@@ -1,15 +1,15 @@
 <?php
 
-$dict = "/Users/limx/Applications/GitHub/limingxinleo/php-jieba-ext/dict/jieba.dict.utf8";
-$hmm = "/Users/limx/Applications/GitHub/limingxinleo/php-jieba-ext/dict/hmm_model.utf8";
-$user = "/Users/limx/Applications/GitHub/limingxinleo/php-jieba-ext/dict/user.dict.utf8";
-$idf = "/Users/limx/Applications/GitHub/limingxinleo/php-jieba-ext/dict/idf.utf8";
-$stop = "/Users/limx/Applications/GitHub/limingxinleo/php-jieba-ext/dict/stop_words.utf8";
+$dict = __DIR__ . "/../dict/jieba.dict.utf8";
+$hmm = __DIR__ . "/../dict/hmm_model.utf8";
+$user = __DIR__ . "/../dict/user.dict.utf8";
+$idf = __DIR__ . "/../dict/idf.utf8";
+$stop = __DIR__ . "/../dict/stop_words.utf8";
 
 $jieba = new PHPJieba($dict, $hmm, $user, $idf, $stop);
 $jieba->insert("知我探索");
 
-$http = new Swoole\Http\Server("127.0.0.1", 9501, SWOOLE_BASE, SWOOLE_SOCK_TCP);
+$http = new Swoole\Http\Server("0.0.0.0", 9501, SWOOLE_BASE, SWOOLE_SOCK_TCP);
 
 $http->set([
     'enable_coroutine' => true,
