3b266625dc39faeb61ec57783f1832e7df2b2dd6
李铭昕 <715557344@qq.com>
2020-06-16 14:33:50 +0800

    Update test.php

--- examples/test.php
+++ examples/test.php
@@ -20,6 +20,7 @@ var_dump($jieba->cutAll('我在知我探索工作'));
 var_dump($jieba->cutAll('和恋人和好'));
 
 var_dump($jieba->cut('我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。'));
+var_dump($jieba->cutAll('我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。'));
 
 var_dump($jieba->cutWithoutTagName('我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。', 'x'));
 
