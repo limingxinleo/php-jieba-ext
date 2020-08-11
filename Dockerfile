9307ae10298ead358d6ded48f83dd678aeb93162
李铭昕 <715557344@qq.com>
2020-08-11 10:46:26 +0800

    Added auto build.

--- Dockerfile
+++ Dockerfile
@@ -6,22 +6,17 @@
 # @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
 
 ARG PHP_VERSION
+ARG ALPINE_VERSION
 
-FROM hyperf/hyperf:${PHP_VERSION}-alpine-v3.9-base
+FROM hyperf/hyperf:${PHP_VERSION}-alpine-v${ALPINE_VERSION}-dev
 
 LABEL maintainer="Hyperf Developers <group@hyperf.io>" version="1.0" license="MIT"
-
-RUN set -ex \
-    && apk update \
-    && apk add autoconf dpkg-dev dpkg file g++ gcc libc-dev make php7-dev php7-pear pkgconf re2c pcre-dev zlib-dev libtool automake
 
 WORKDIR /opt/www
 
 ADD . .
 
-RUN ln -s /usr/bin/phpize7 /usr/local/bin/phpize \
-    && ln -s /usr/bin/php-config7 /usr/local/bin/php-config \
-    && g++ -o libjieba.so -fPIC -c -DLOGGING_LEVEL=LL_WARNING -I./include/ lib/jieba.cpp \
+RUN g++ -o libjieba.so -fPIC -c -DLOGGING_LEVEL=LL_WARNING -I./include/ lib/jieba.cpp \
     && phpize \
     && ./configure \
     && make
--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env sh
 
-export PHP_VERSION=7.2 && docker-compose build jieba-ext
-docker run --name jieba7.2 hyperf/jieba:7.2
-docker cp jieba7.2:/opt/www/modules/jieba.so build/jieba-7.2.so
-docker rm jieba7.2
+mkdir "build"
 
-export PHP_VERSION=7.3 && docker-compose build jieba-ext
-docker run --name jieba7.3 hyperf/jieba:7.3
-docker cp jieba7.3:/opt/www/modules/jieba.so build/jieba-7.3.so
-docker rm jieba7.3
+export PHP_VERSION=7.2 && export ALPINE_VERSION=3.9 && docker-compose build jieba-ext
+docker run --name jieba7.2-v3.9 hyperf/jieba:7.2-v3.9
+docker cp jieba7.2-v3.9:/opt/www/modules/jieba.so build/jieba-7.2-alpine-v3.9.so
+docker rm jieba7.2-v3.9
 
-export PHP_VERSION=7.4 && docker-compose build jieba-ext
-docker run --name jieba7.4 hyperf/jieba:7.4
-docker cp jieba7.4:/opt/www/modules/jieba.so build/jieba-7.4.so
-docker rm jieba7.4
+PHPS="7.3 7.4"
+ALPINES="3.9 3.10 3.11"
+
+for PHP_VERSION in ${PHPS} ; do
+  for ALPINE_VERSION in ${ALPINES} ; do
+    export PHP_VERSION=${PHP_VERSION} && export ALPINE_VERSION=${ALPINE_VERSION} && docker-compose build jieba-ext
+    docker run --name jieba${PHP_VERSION}-v${ALPINE_VERSION} hyperf/jieba:${PHP_VERSION}-v${ALPINE_VERSION}
+    docker cp jieba${PHP_VERSION}-v${ALPINE_VERSION}:/opt/www/modules/jieba.so build/jieba-${PHP_VERSION}-alpine-v${ALPINE_VERSION}.so
+    docker rm jieba${PHP_VERSION}-v${ALPINE_VERSION}
+  done
+done
